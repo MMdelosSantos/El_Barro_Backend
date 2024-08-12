69d21c06319cfabfb726e84d2e2fd15115466649
MMdelosSantos <magdalenadls@outlook.com>
2024-08-11 22:03:26 -0300

    Carrito y productos operativos

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 const productsRouter = require('./routes/products.router.js');
 const cartsRouter = require('./routes/carts.router.js');
 const ProductsManager = require('./dao/ProductsManager.js');
-const CartsManager= require('./dao/CartsManager.js');
+const CartsManager = require('./dao/CartsManager.js');
 // Importando express js
 
 const express = require('express');
@@ -12,16 +12,16 @@ const app = express();
 
 //Configurando para que interprete mensajes JSON
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-ProductsManager.path="./src/data/products.json";
-CartsManager.path="./src/data/carts.json";
+ProductsManager.path = "./src/data/products.json";
+CartsManager.path = "./src/data/carts.json";
 // Creando el servidor 
-const PORT=8080
+const PORT = 8080
 
-app.get("/", (req, res)=> {
-    res.setHeader('Content-Type','text/plain');
-    res.status(200).send("Home Page" )
+app.get("/", (req, res) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(200).send("Home Page")
 })
 
 app.use("/api/products", productsRouter);
@@ -30,4 +30,4 @@ app.use("/api/carts", cartsRouter);
 
 
 
-app.listen(PORT, () =>  console.log(`Escuchando en puerto ${PORT}`));
+app.listen(PORT, () => console.log(`Escuchando en puerto ${PORT}`));
--- src/dao/CartsManager.js
+++ src/dao/CartsManager.js
@@ -13,19 +13,56 @@ class CartsManager {
         }
     }
 
-    static async create() {
-        let carts=await this.getCarts()
-        let id=1;
-        if(carts.length>0){
-            id=Math.max(...carts.map(d=>d.id))+1;
-        }
-        carts.push({
-            id,
-            products
-        })
+    static async create(products = []) { // Método para generar un carrito nuvo
+        let carts = await this.getCarts();
 
-await fs.promises.writeFile(this.path, JSON.stringify(carts, null, 5))
-return {id,products}
+        let id = 1;
+        if (carts.length > 0) {
+            id = Math.max(...carts.map(d => d.id)) + 1;
+        }
+
+        products.forEach(p => {
+            if (!p.id || typeof p.quantity !== 'number' || p.quantity <= 0) {
+                throw new Error('Cada producto debe tener un productId válido y una cantidad mayor a 0');
+            }
+        });
+
+        const newCart = {
+            id: id,
+            products: products
+        };
+
+        carts.push(newCart);
+
+        await fs.promises.writeFile(this.path, JSON.stringify(carts, null, 5));
+        return newCart;
     }
+
+
+    static async update(id, cart = {}) { // Método para incorporar un producto a un carrito // REVISAR
+        let carts = await this.get()
+        let indexCart = carts.findIndex(c => c.d === id)
+        if (indexCart === -1) {
+            throw new Error(`No existe carrito con id${id}`)
+        }
+        carts[indexCart] = cart
+        await fs.promises.writeFile(this.path, JSON.stringify(carts, null, 5))
+        return carts[indexCart]
+
+    }
+
+    static async getCartById(cartId) { // Método para filtrar un carrito por su id
+        let carts = await this.getCarts();
+        let cart = carts.find(c => c.id === cartId);
+
+        if (!cart) {
+            throw new Error(`Carrito con id ${cartId} no encontrado`);
+        }
+
+        return cart;
+    }
+
+
 }
-    module.exports = CartsManager;
+
+module.exports = CartsManager;
